1c3b2f2b76fc73d4ec4d4683b3fe0b331a6a3129
vitor-driven <vitor_henrique001@hotmail.com>
2023-07-31 11:43:55 -0300

    xis dede

--- src/routers/customersRouter.js
+++ src/routers/customersRouter.js
@@ -12,6 +12,6 @@ const customersRouter = express.Router();
 
 customersRouter.get("/customers", getCusts);
 customersRouter.get("/customers/:id", searchCust);
-customersRouter.post("/addCust", validateCust, addCust);
-customersRouter.put("/updateCust/:id", validateCust, updateCust);
+customersRouter.post("/customers", validateCust, addCust);
+customersRouter.put("/customers/:id", validateCust, updateCust);
 export default customersRouter;
