1ebe29cb1a13e8f976e41e80421a3dd0b31700c6
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-07-31 06:42:04 -0300

    fix conexão do db

--- src/db.js
+++ src/db.js
@@ -8,6 +8,12 @@ const configDatabase = {
     connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+if (process.env.MODE === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
+
+db.connect()
+    .then(() => {
+        console.log("Conectado ao PostgreSQL");
+    })
+    .catch((err) => console.error("Erro ao conectar com PostgreSQL:", err));
